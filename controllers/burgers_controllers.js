ac6a53a10b41da7d70cf6e6f91f24009bc539a42
jmauro33 <jonathanmauro16@yahoo.com>
2020-03-11 14:28:53 -0500

    initial edits to js code

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -2,32 +2,32 @@ var express = require("express");
 
 var router = express.Router();
 
-// Import the model (cat.js) to use its database functions.
-var cat = require("../models/cat.js");
 
-// Create all our routes and set up logic within those routes where required.
+var cat = require("../models/burgers.js");
+
+
 router.get("/", function(req, res) {
   cat.all(function(data) {
     var hbsObject = {
-      cats: data
+      burgers: data
     };
     console.log(hbsObject);
     res.render("index", hbsObject);
   });
 });
 
-router.post("/api/cats", function(req, res) {
+router.post("/api/burgers", function(req, res) {
   cat.create([
-    "name", "sleepy"
+    "name", "eat"
   ], [
     req.body.name, req.body.sleepy
   ], function(result) {
-    // Send back the ID of the new quote
+   
     res.json({ id: result.insertId });
   });
 });
 
-router.put("/api/cats/:id", function(req, res) {
+router.put("/api/burgers/:id", function(req, res) {
   var condition = "id = " + req.params.id;
 
   console.log("condition", condition);
@@ -36,7 +36,7 @@ router.put("/api/cats/:id", function(req, res) {
     sleepy: req.body.sleepy
   }, condition, function(result) {
     if (result.changedRows == 0) {
-      // If no rows were changed, then the ID must not exist, so 404
+ 
       return res.status(404).end();
     } else {
       res.status(200).end();
@@ -44,12 +44,12 @@ router.put("/api/cats/:id", function(req, res) {
   });
 });
 
-router.delete("/api/cats/:id", function(req, res) {
+router.delete("/api/burger/:id", function(req, res) {
   var condition = "id = " + req.params.id;
 
   cat.delete(condition, function(result) {
     if (result.affectedRows == 0) {
-      // If no rows were changed, then the ID must not exist, so 404
+      
       return res.status(404).end();
     } else {
       res.status(200).end();
@@ -57,5 +57,5 @@ router.delete("/api/cats/:id", function(req, res) {
   });
 });
 
-// Export routes for server.js to use.
+
 module.exports = router;
